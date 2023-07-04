df75708624180892263cfe38b5eb0a4d45aa3ad2
ewen.zhang <ewen.zhang@welab-inc.com>
2023-07-04 04:21:40 +0100

    增加l4d2-manager部分代码

---
+++ middlewares/auth.go
@@ -0,0 +1,22 @@
+package middlewares
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Auth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		password := c.PostForm("password")
+		realPassword := os.Getenv("L4D2_MANAGER_PASSWORD")
+
+		if password != realPassword {
+			c.String(http.StatusBadRequest, "密码错误")
+			c.Abort()
+		}
+
+		c.Next()
+	}
+}
