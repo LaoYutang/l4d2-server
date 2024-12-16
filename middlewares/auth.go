4e1347b66da6153cf8ac4e5ed54cb964efc15888
LaoYutang <539870188@qq.com>
2024-12-16 16:21:56 +0800

    feat 增加单个文件删除功能

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -11,6 +11,9 @@ func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		password := c.PostForm("password")
 		realPassword := os.Getenv("L4D2_MANAGER_PASSWORD")
+		if realPassword == "" {
+			realPassword = "laoyutangnb"
+		}
 
 		if password != realPassword {
 			c.String(http.StatusBadRequest, "密码错误")
