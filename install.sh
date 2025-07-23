d67338c1e673a184c14b411363837ddb8085ef30
LaoYutang <539870188@qq.com>
2025-07-23 09:43:29 +0800

    feat 更新文档、安装脚本适配tick

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ if ! command -v docker &> /dev/null; then
 fi
 
 # 询问管理密码并确认
-read -s -p "请设置管理密码: " admin_password
+read -r -s -p "请设置管理密码: " admin_password
 echo
-read -s -p "请确认管理密码: " admin_password_confirm
+read -r -s -p "请确认管理密码: " admin_password_confirm
 echo
 
 if [ "$admin_password" != "$admin_password_confirm" ]; then
@@ -19,11 +19,11 @@ if [ "$admin_password" != "$admin_password_confirm" ]; then
 fi
 
 # 询问游戏端口，默认为27015
-read -p "请输入游戏端口 (默认: 27015): " game_port
+read -r -p "请输入游戏端口 (默认: 27015): " game_port
 game_port=${game_port:-27015}
 
 # 询问管理面板端口，默认为27020
-read -p "请输入管理面板端口 (默认: 27020): " manager_port
+read -r -p "请输入管理面板端口 (默认: 27020): " manager_port
 manager_port=${manager_port:-27020}
 
 mkdir -p /data/l4d2
@@ -38,7 +38,7 @@ networks:
 
 services:
   l4d2:
-    image: laoyutang/l4d2:latest
+    image: laoyutang/l4d2:nightly
     container_name: l4d2
     ports:
       - "$game_port:27015"
@@ -48,6 +48,10 @@ services:
       - cfg:/l4d2/left4dead2/cfg
     networks:
       - l4d2-network
+    environment:
+      - L4D2_TICK=60 # 30,60,100
+    depends_on:
+      - l4d2-manager
 
   l4d2-manager:
     image: laoyutang/l4d2-manager:latest
