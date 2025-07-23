19007a61afabe94aabf6d2176dfbd2dfaf51d74b
LaoYutang <539870188@qq.com>
2025-07-23 09:29:14 +0800

    feat 增加环境变量控制tickrate

--- start.sh
+++ start.sh
@@ -41,7 +41,23 @@ else
     echo "cfg目录不为空，跳过复制"
 fi
 
+# 控制tick配置文件，环境变量L4D2_TICK
+REAL_TICK=${L4D2_TICK:-30}  # 默认值为30
+if [ "${L4D2_TICK}" = "60" ]; then
+    echo "设置tickrate为60"
+    cp /l4d2/left4dead2/cfg/server.cfg.60tick /l4d2/left4dead2/cfg/server.cfg
+    REAL_TICK=60
+elif [ "${L4D2_TICK}" = "100" ]; then
+    echo "设置tickrate为100"
+    cp /l4d2/left4dead2/cfg/server.cfg.100tick /l4d2/left4dead2/cfg/server.cfg
+    REAL_TICK=100
+else
+    echo "设置tickrate为30"
+    cp /l4d2/left4dead2/cfg/server.cfg.30tick /l4d2/left4dead2/cfg/server.cfg
+fi
+
+
 echo "文件检查和初始化完成，启动服务器..."
 
 # 启动L4D2服务器
-cd /l4d2 && ./srcds_run -game left4dead2 -insecure -condebug +hostport 27015 +exec server.cfg
+cd /l4d2 && ./srcds_run -game left4dead2 -insecure -tickrate "${REAL_TICK}" -condebug +hostport 27015 +exec server.cfg
